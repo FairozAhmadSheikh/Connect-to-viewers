51094cce0ae9d3ba08dec5daaca8ad9f613b2c3f
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-11-11 21:50:27 +0530

    vercel changes made

--- app.py
+++ app.py
@@ -165,4 +165,4 @@ def api_messages():
         })
     return jsonify(out)
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
